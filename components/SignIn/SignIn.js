2f02123010a9c35765372fac9338cb16c9e3a7a4
Mohammad anas khan <muhammedanas5186@gmail.com>
2023-06-05 09:10:02 -0700

    :art: form button designed for Desktop View

--- components/SignIn/SignIn.js
+++ components/SignIn/SignIn.js
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from "/components/SignIn/SignIn.module.css"
 
 function SignIn(props) {
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
     var {toggleSignInUp} = props;
 
     const toggleSignUp = () => {
         toggleSignInUp()
     }
+
+    async function authHandler(e) {
+      // setLoading(true);
+      // e.preventDefault();
+    }
+
+    async function demoUserSignIn(e) {
+      setLoading(true);
+      e.preventDefault();
+      //logIn("test@test.com", "123456789");
+  }
+
   return (
     <div  className={styles.main}>
-        <button onClick={toggleSignUp} color='black'>Sign Up</button>
+      <div className={styles.auth_heading}>
+        <h3>Create Your Account</h3>
+      </div>
+      <form onSubmit={authHandler}>
+        <div className={styles.form_input_group}>
+            <input type="email" placeholder="Email" ref={emailRef} />
+        </div>
+        <div className={styles.form_input_group}>
+            <input type="password" placeholder="Password" ref={passwordRef} min="6" />
+        </div>
+        <div className={styles.form_input_group}>
+            <button className={styles.btn}>Sign In</button>
+        </div>
+        <div className={styles.form_input_group}>
+            <button onClick={demoUserSignIn} className={styles.btn}>Demo User</button>
+        </div>
+      </form>
     </div>
   )
 }

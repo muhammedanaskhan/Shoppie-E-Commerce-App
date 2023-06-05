e6fa7f539c8aabcb332c86ebe8e7afeed0f94534
Mohammad anas khan <muhammedanas5186@gmail.com>
2023-06-05 14:42:23 -0700

    :art: both forms designed

--- components/SignIn/SignIn.js
+++ components/SignIn/SignIn.js
@@ -27,7 +27,7 @@ function SignIn(props) {
     <div  className={styles.main}>
       <div className={styles.formContainer}>
           <div className={styles.auth_heading}>
-            <h3>Create Your Account</h3>
+            <h3>Log In</h3>
           </div>
           <form onSubmit={authHandler}>
             <div className={styles.form_input_group}>
@@ -37,12 +37,15 @@ function SignIn(props) {
                 <input type="password" placeholder="Password" ref={passwordRef} min="6" />
             </div>
             <div className={styles.form_input_group}>
-                <button className={styles.btn}>Sign In</button>
+                <button className={styles.btn}>LOG IN</button>
             </div>
             <div className={styles.form_input_group}>
-                <button onClick={demoUserSignIn} className={styles.btn}>Demo User</button>
+                <button onClick={demoUserSignIn} className={styles.btn}>DEMO USER</button>
             </div>
           </form>
+      </div>
+      <div className={styles.auth_change}>  
+        <p>Don't have an account? <span onClick={toggleSignUp}>Sign Up</span></p>
       </div>
     </div>
   )
--- components/SignUp/SignUp.js
+++ components/SignUp/SignUp.js
@@ -1,16 +1,50 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from "/components/SignUp/SignUp.module.css"
 
 function SignUp(props) {
+
+  
+  const emailRef = useRef();
+  const passwordRef = useRef();
     var {toggleSignInUp} = props;
 
     const toggleSignIn = () => {
         toggleSignInUp()
     }
+    async function authHandler(e) {
+      // setLoading(true);
+      // e.preventDefault();
+    }
 
+    async function demoUserSignIn(e) {
+      setLoading(true);
+      e.preventDefault();
+      //logIn("test@test.com", "123456789");
+  }
   return (
-    <div className={styles.main}>
-        <button onClick={toggleSignIn} color='black'>SignIn</button>
+    <div  className={styles.main}>
+      <div className={styles.formContainer}>
+          <div className={styles.auth_heading}>
+            <h3>Create Your Account</h3>
+          </div>
+          <form onSubmit={authHandler}>
+            <div className={styles.form_input_group}>
+                <input type="email" placeholder="Email" ref={emailRef} />
+            </div>
+            <div className={styles.form_input_group}>
+                <input type="password" placeholder="Password" ref={passwordRef} min="6" />
+            </div>
+            <div className={styles.form_input_group}>
+                <button className={styles.btn}>CREATE ACCOUNT</button>
+            </div>
+            <div className={styles.form_input_group}>
+                <button onClick={demoUserSignIn} className={styles.btn}>DEMO USER</button>
+            </div>
+          </form>
+      </div>
+      <div className={styles.auth_change}>  
+        <p>Already have an account? <span onClick={toggleSignIn}>Sign In</span></p>
+      </div>
     </div>
   )
 }

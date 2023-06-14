0ea12141836c92a44e79f5b2f00a34b39606e38c
Mohammad anas khan <muhammedanas5186@gmail.com>
2023-06-14 11:26:25 -0700

    :sparkles: Problem with this route protection method (client side code)

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -9,10 +9,17 @@ function index() {
             window.location.href = '/signIn'
         }
     },[])
-    
+
   return (
     <div>cart</div>
   )
 }
 
 export default index
+
+// The Problem with just this client side code is that 
+// it visit Cart Route and checks for session, which takes 
+// a fraction of a second of stay at cart page and then again
+// redirects to signIn page
+
+// we don't want that two reloads -- bad ux

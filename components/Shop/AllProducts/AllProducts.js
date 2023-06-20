bd2e0b523623b178ace0e8ba94e2aef1c626036b
Mohammad anas khan <muhammedanas5186@gmail.com>
2023-06-20 11:37:53 -0700

    :art: banner for shop, products data added

---
+++ components/Shop/AllProducts/AllProducts.js
@@ -0,0 +1,13 @@
+import classes from './AllProducts.module.css'
+
+import React from 'react'
+
+function AllProducts() {
+  return (
+    <div className={classes.banner}>
+        <h1 className={classes.slogan}>Walk in for the Fashion, <br/>Stay in for the Style.</h1>
+    </div>
+  )
+}
+
+export default AllProducts
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../../components/Nav/Header/Header'
+import AllProducts from '../../../components/Shop/AllProducts/AllProducts'
 
 function index() {
   return (
     <>
         <Header/>
+        <AllProducts/>
     </>
   )
 }
